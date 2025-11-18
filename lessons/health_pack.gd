36044d96c149c4f781cf85a856e569668d78c515
Fluffybihno <huvo-tang@s.sfusd.edu>
2025-11-18 14:06:40 -0800

    M5 lesson 3 complete

--- lessons/health_pack.gd
+++ lessons/health_pack.gd
@@ -1,10 +1,7 @@
 extends Area2D
 
-func _on_area_entered(area_that_entered: Area2D) -> void:
-	pass
-
 func _ready() -> void:
-	area_entered.connect(delete_the_health_pack)
-	
-func delete_the_health_pack(what_touched_the_health_pack:Area2D) -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
 	queue_free()
